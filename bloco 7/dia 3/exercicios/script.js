74cf2f58abe1eebdb95be4df438cc35d1d531362
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-29 11:02:44 -0300

    exercicio1 part2

--- bloco 7/dia 3/exercicios/script.js
+++ bloco 7/dia 3/exercicios/script.js
@@ -123,3 +123,30 @@ const obj3 = {
 
 assert.deepStrictEqual(obj1, obj2, 'os objetos devem ser igual')
 assert.notDeepStrictEqual(obj1, obj3, 'os objetos devem ser diferentes');
+
+
+// Praticando TDD - Parte 2
+// Nessa parte os exercícios estão divididos em dois grupos: primeiro, você vai escrever código baseado nos testes. Depois você lerá um código e o que ele tem que trazer de resposta. A partir disso, você escreverá testes e os usará de base para alterar o código. Como assim? Bem, vamos passo a passo!
+// Escrevendo código para testes
+// Dados os casos de testes abaixo, escreva as funções de forma a passar nos testes. É importante nunca alterar os testes ou as variáveis já escritas no código :
+// Copie os testes já implementados e desenvolva as funções. Separe as funções em arquivos para evitar qualquer tipo de problema.
+// Escreva a função addOne para passar nos testes já implementados.
+
+const assert = require('assert');
+// escreva a função addOne aqui
+const addOneArray = [];
+const addOne = () => {
+  for( let i = 0; i < myArray.length; i += 1) {
+    addOneArray.push(myArray[i] + 1);
+  }
+  return addOneArray;
+}
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
