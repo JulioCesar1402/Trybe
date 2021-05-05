dd3907b45ed75563d51883f2507b09937b1cb835
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-05-05 18:51:54 -0300

    ex1 refac

--- bloco_8/day-4/exercises/exercise1.js
+++ bloco_8/day-4/exercises/exercise1.js
@@ -8,10 +8,8 @@ const arrays = [
 ];
 
 function flatten() {
-  // escreva seu código aqui
   // Dada uma matriz de matrizes, transforme em uma única matriz.
   const arraySum = arrays.reduce((accumulator, current) => accumulator.concat(current));
   return arraySum
 }
-// console.log(flatten());
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
